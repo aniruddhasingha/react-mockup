aade6658bb125bc4c70820f859f8b7cc862f9a7b
aniruddhasingha <aniruddhasingha95@gmail.com>
2020-06-17 18:20:34 +0530

    added react-router-dom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,43 +1,49 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Switch, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
-import Posts from '../Blog/Posts/Posts'
-import NewPost from '../Blog/NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost')
+});
 class Blog extends Component {
+    state = {
+        auth: true
+    }
     render() {
-
-
         return (
             <div className="Blog">
-                <header >
+                <header>
                     <nav>
-                        <ul >
-                            <li><NavLink activeStyle={{
-                                color: '#fa923f',
-                                textDecoration: 'underline'
-                            }} activeClassName="my-active" to="/" exact>Home</NavLink></li>
-                            <li><NavLink activeStyle={{
-                                color: '#fa923f',
-                                textDecoration: 'underline'
-                            }} to={{
-                                pathname: "/new-post",
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }} exact
-                                activeClassName="my-active"
-                            >New Post</NavLink></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/" exact component={Posts} />
-                    <Route path="/new-post" exact component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    <Route render={() => <h1>Not Found</h1>} />
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
-
             </div>
         );
     }
